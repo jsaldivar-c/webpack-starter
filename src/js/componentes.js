f7747d422bdab688b518dc0710870ef9aea29a22
Jose Saldivar <contacto@josesaldivarc.com>
2020-07-07 13:44:40 -0500

    Modificación del titulo en  H1

--- src/js/componentes.js
+++ src/js/componentes.js
@@ -5,6 +5,7 @@ export const saludar = (nombre) => {
     console.log('Creando H1');
     
     const h1 = document.createElement('h1');
-    h1.innerText = `Hola ${nombre}, bienvenido a Webpack.`;
+    //h1.innerText = `Hola ${nombre}, bienvenido a Webpack.`;
+    h1.innerText = `Hola, bienvenido a Webpack.`;
     document.body.append(h1);
 }
